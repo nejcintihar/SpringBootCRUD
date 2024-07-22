a9e254acc8ddc724763e6d907e320b890140b799
nintihar <nejc.inti21@gmail.com>
2024-07-22 11:34:57 +0200

    feat: optimize imports, add doc comments

--- src/main/java/com/dev/product/GlobalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/dev/product/GlobalExceptionHandler/GlobalExceptionHandler.java
@@ -10,35 +10,52 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+/**
+ * Global exception handler class that handles exceptions across the application.
+ */
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * Handles the ResourceNotFoundException and returns a custom error response.
+     *
+     * @param ex the ResourceNotFoundException instance
+     * @return a ResponseEntity with the custom error response and HTTP status NOT_FOUND
+     */
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
-        CustomErrorResponse errorResponse = new CustomErrorResponse();
-        errorResponse.setTimestamp(LocalDateTime.now().toString());
-        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-        errorResponse.setError("Resource Not Found");
-        errorResponse.setDetails(List.of(ex.getMessage()));
+        CustomErrorResponse errorResponse = new CustomErrorResponse(
+                LocalDateTime.now().toString(),
+                HttpStatus.NOT_FOUND.value(),
+                "Resource Not Found",
+                List.of(ex.getMessage())
+        );
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
+    /**
+     * Handles the MethodArgumentNotValidException and returns a custom error response.
+     *
+     * @param ex the MethodArgumentNotValidException instance
+     * @return a ResponseEntity with the custom error response and HTTP status BAD_REQUEST
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<CustomErrorResponse> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        List<String> details = new ArrayList<>();
-        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
-            details.add(error.getField() + ": " + error.getDefaultMessage());
-        }
+        List<String> details = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.toList());
 
-        CustomErrorResponse errorResponse = new CustomErrorResponse();
-        errorResponse.setTimestamp(LocalDateTime.now().toString());
-        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-        errorResponse.setError("Validation Failed");
-        errorResponse.setDetails(details);
+        CustomErrorResponse errorResponse = new CustomErrorResponse(
+                LocalDateTime.now().toString(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation Failed",
+                details
+        );
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
