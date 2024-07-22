005e03996fc408b1c007f3fc827f43618a374bdb
nintihar <nejc.inti21@gmail.com>
2024-07-22 11:32:21 +0200

    feat: optimize imports, add doc comments, lombok

--- src/main/java/com/dev/product/ErrorResponse/CustomErrorResponse.java
+++ src/main/java/com/dev/product/ErrorResponse/CustomErrorResponse.java
@@ -1,65 +1,57 @@
 package com.dev.product.ErrorResponse;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.ErrorResponse;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * Custom error response class that implements the ErrorResponse interface.
+ * Represents the structure of error responses returned by the application.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomErrorResponse implements ErrorResponse {
+    /**
+     * The timestamp of when the error occurred.
+     */
     private String timestamp;
+
+    /**
+     * The HTTP status code of the error response.
+     */
     private int status;
+
+    /**
+     * The error message or title.
+     */
     private String error;
+
+    /**
+     * The list of details or additional information about the error.
+     */
     private List<String> details;
 
-    public CustomErrorResponse() {
-    }
-
-    public CustomErrorResponse(String timestamp, int status, String error, List<String> details) {
-        this.timestamp = timestamp;
-        this.status = status;
-        this.error = error;
-        this.details = details;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
-    }
-
-    public List<String> getDetails() {
-        return details;
-    }
-
-    public void setDetails(List<String> details) {
-        this.details = details;
-    }
-
+    /**
+     * Returns the HTTP status code of the error response.
+     *
+     * @return the HttpStatus object representing the status code
+     */
     @Override
     public HttpStatus getStatusCode() {
         return HttpStatus.valueOf(status);
     }
 
+    /**
+     * Returns the ProblemDetail object representing the error response body.
+     *
+     * @return the ProblemDetail object containing the error details
+     */
     @Override
     public ProblemDetail getBody() {
         ProblemDetail problemDetail = ProblemDetail.forStatus(getStatusCode());
